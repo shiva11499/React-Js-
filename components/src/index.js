25c15f260cc8381722df23c8067646ab7d43b160
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-11 16:14:35 +0530

    Add. Embed Comment in Approval Card

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,38 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Shivansh"
-        time="Today at 5:25PM"
-        content="Nice post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Aayushi"
-        time="Today at 7:45PM"
-        content="Great job!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Ayush"
-        time="Yesterday at 11:30AM"
-        content="Could be a little better"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Caution!</h4>
+          Are you sure?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Shivansh"
+          time="Today at 5:25PM"
+          content="Nice post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Aayushi"
+          time="Today at 7:45PM"
+          content="Great job!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Ayush"
+          time="Yesterday at 11:30AM"
+          content="Could be a little better"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
