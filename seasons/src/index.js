3da7fdf21d19099ffa69e5f2337e36253e86f26b
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-14 16:42:26 +0530

    refactoring functional component to class based

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,8 +5,14 @@ if (module.hot) {
   module.hot.accept();
 }
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
