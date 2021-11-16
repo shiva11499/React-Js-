be862667b3d2a904e432b35edbf7ec60bb304c8b
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-16 09:40:28 +0530

    Making a GET request with axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {}
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID hLX7GzGOGf9WWd17txgbJwaTiY8cWv7mNTN5wbr5n7Y",
+      },
+    });
+  }
 
   render() {
     return (
