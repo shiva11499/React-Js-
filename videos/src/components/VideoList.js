9bd440f5a835b2be78571259d8e1dbf37ec3e595
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-21 13:56:00 +0530

    Sending the selected video to the App via callback

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from "./VideoItem";
 
 const VideoList = (props) => {
   const renderedList = props.videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem onVideoSelect={props.onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
