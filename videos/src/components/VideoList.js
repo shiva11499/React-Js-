8fb7106d4175c48a766055e60932c1665555e079
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-21 13:43:18 +0530

    Styling VideoList

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+  const renderedList = props.videos.map((video) => {
+    return <VideoItem video={video} />;
+  });
+
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
