7100b7a1f8bdef066123caa6fa97ede217f45ea5
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-21 18:43:35 +0530

    Controlling expansion of Accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -7,7 +7,7 @@ const items = [
     content: "React is a frontend javascript framework",
   },
   {
-    title: "Why use React",
+    title: "Why use React?",
     content: "React is a favorite JS library used among developers",
   },
   {
