f0f2fbc8e927b3927ca80949f1b6e6f975f2d920
Shivansh Garg <shivanshgarg@Shivanshs-MacBook-Pro.local>
2021-11-21 21:34:13 +0530

    Text inputs with hooks

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            type="text"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
